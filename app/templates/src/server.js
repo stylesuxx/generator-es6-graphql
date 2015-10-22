a950531ee978415b6055b60dda5c08ad4471e627
stylesuxx <stylesuxx@gmail.com>
2015-10-22 16:40:58 +0200

    added graphql dependencies

--- app/templates/src/server.js
+++ app/templates/src/server.js
@@ -1,1 +1,18 @@
-console.log('In server.js');
+import graphqlHTTP from 'express-graphql';
+import express from 'express';
+import 'babel/polyfill';
+import path from 'path';
+
+const port = (global.process.env.NODE_ENV == 'develop') ? 1234 : 8080;
+const server = global.server = express();
+
+server.set('port', port);
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(server.get('port'), () => {
+  if (process.send) {
+    process.send('online');
+  } else {
+    console.log('The server is running at http://localhost:' + server.get('port'));
+  }
+});
