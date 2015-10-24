e907cea6c012986d2bef4edc8db9e1d5bc5d363a
stylesuxx <stylesuxx@gmail.com>
2015-10-25 00:08:03 +0200

    Added routes for google auth, fixed some formatting to get rid of empty lines where they do not belong

--- app/templates/src/server.js
+++ app/templates/src/server.js
@@ -2,35 +2,39 @@ import { ItemSchema } from './schema/itemSchema';
 import graphqlHTTP from 'express-graphql';
 import express from 'express';
 import 'babel/polyfill';
-import path from 'path';
-
-<% if (database === 'mongoose') { %>import mongoose from 'mongoose';<% } %>
-<% if (authentication) { %>import passport from './passport';
+import path from 'path';<% if (database === 'mongoose') { %>
+import mongoose from 'mongoose';<% } %><% if (authentication) { %>
+import passport from './passport';
 import session from 'express-session';<% } %>
 
 const port = (global.process.env.NODE_ENV == 'develop') ? 1234 : 8080;
-const server = global.server = express();
+const server = global.server = express();<% if (database === 'mongoose') { %>
 
-<% if (database === 'mongoose') { %>mongoose.connect('mongodb://localhost/<%= appname %>');<% } %>
+mongoose.connect('mongodb://localhost/<%= appname %>');<% } %>
 
 server.set('port', port);
-server.use(express.static(path.join(__dirname, 'public')));
-<% if (authentication) { %>
+server.use(express.static(path.join(__dirname, 'public')));<% if (authentication) { %>
 server.use(passport.initialize());
 server.use(passport.session());
 server.use(session({
   secret: '<%= secret %>',
   resave: true,
   saveUninitialized: true
-}));
-<% } %>
-<% authFull.forEach(function(auth) { %>server.get('/auth/<%= auth.slug %>', passport.authenticate('<%= auth.slug %>'));
-server.get('/auth/<%= auth.slug %>/callback', passport.authenticate('<%= auth.slug %>', {
+}));<% if (auth.indexOf('passport-github') > -1) { %>
+
+server.get('/auth/github', passport.authenticate('github'));
+server.get('/auth/github/callback', passport.authenticate('github', {
   successRedirect: '/',
   failureRedirect: '/login'
-}));
+}));<% }; %><% if (auth.indexOf('passport-google-oauth') > -1) { %>
 
-<% }); %>server.use('<%= graphqlroute %>', graphqlHTTP({schema: ItemSchema, graphiql: <%= graphiql %>}));
+server.get('/auth/google', passport.authenticate('google', {scope: 'https://www.google.com/m8/feeds'}));
+server.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));<% }; %>
+<% } %>
+server.use('<%= graphqlroute %>', graphqlHTTP({schema: ItemSchema, graphiql: <%= graphiql %>}));
 
 server.listen(server.get('port'), () => {
   if (process.send) {
