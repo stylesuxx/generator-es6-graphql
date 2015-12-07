5dbef21e924bcab77af99bd005c048b341723423
stylesuxx <stylesuxx@gmail.com>
2015-12-07 22:43:07 +0100

    Added skeleton for local auth

--- app/templates/src/passport.js
+++ app/templates/src/passport.js
@@ -4,9 +4,10 @@ import passport<%- auth.name %> from '<%- auth.npm %>';<% }); %><%if (auth.lengt
 <% } %><% if (auth.indexOf('passport-github') > -1) { %>
 const GithubStrategy = passportGithub.Strategy;<% } %><% if (auth.indexOf('passport-google-oauth') > -1) { %>
 const GoogleStrategy = passportGoogle.OAuth2Strategy;<% } %><% if (auth.indexOf('passport-facebook') > -1) { %>
-const FacebookStrategy = passportFacebook.Strategy;<% } %><%if (auth.length > 0) { %>
+const FacebookStrategy = passportFacebook.Strategy;<% } %><% if (auth.indexOf('passport-local') > -1) { %>
+const LocalStrategy = passportLocal.Strategy;<% } %><%if (auth.length > 0) { %>
 <% } %>
-<% authFull.forEach(function(auth){ %>passport.use(new <%- auth.name %>Strategy(
+<% authFull.forEach(function(auth){ if(auth.slug !== 'local') {%>passport.use(new <%- auth.name %>Strategy(
   {
     clientID: config.<%- auth.slug %>.id,
     clientSecret: config.<%- auth.slug %>.secret,
@@ -17,7 +18,19 @@ const FacebookStrategy = passportFacebook.Strategy;<% } %><%if (auth.length > 0)
   }
 ));
 
-<% }); %>passport.serializeUser(function(user, done) {
+<% } else {%>passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user || !user.validPassword(password)) {
+        return done(null, false, { message: 'Username and password do not match.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+<%} }); %>passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
