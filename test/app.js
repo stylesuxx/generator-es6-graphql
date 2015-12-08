90f35768f7a3bf690a2514cb833057e6d84c1ec8
stylesuxx <stylesuxx@gmail.com>
2015-12-09 00:20:01 +0100

    Added testcases to test with enabled oAuth strategies

--- test/app.js
+++ test/app.js
@@ -54,4 +54,53 @@ describe('generator:app', function () {
       assert.fileContent('package.json',  /"name": "temp"/);
     });
   });
+
+  describe('oAuth', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../app'))
+        .withPrompts({
+          name: 'temp',
+          auth: ['passport-facebook', 'passport-github', 'passport-google-oauth']
+        })
+        .on('end', done);
+    });
+
+    it('creates files', function () {
+      var expected = [
+        'package.json',
+        '.babelrc',
+        '.eslintrc',
+        '.travis.yml',
+        'README.md',
+        '.gitignore',
+        'src/server.js',
+        'public/.placeholder',
+        'tools/lib/copy.js',
+        'tools/build.js',
+        'tools/bundle.js',
+        'tools/clean.js',
+        'tools/config.js',
+        'tools/copy.js',
+        'tools/serve.js',
+        'tools/start.js',
+        'src/lib/items.js',
+        'src/schema/items.js',
+        'src/schema/index.js',
+        'src/passport.js',
+        'src/passportConfig.js'
+      ];
+
+      assert.file(expected);
+    });
+
+    it('fills package.json with correct information', function () {
+      assert.fileContent('package.json',  /"name": "temp"/);
+    });
+
+    it('fills src/passportConfig.js with correct information', function () {
+      assert.fileContent('src/passportConfig.js',  /facebook:/);
+      assert.fileContent('src/passportConfig.js',  /github:/);
+      assert.fileContent('src/passportConfig.js',  /google:/);
+    });
+  });
 });
