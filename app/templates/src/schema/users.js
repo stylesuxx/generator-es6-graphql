cc4dc192da08de0dd728ef89a3577dd007e141be
stylesuxx <stylesuxx@gmail.com>
2015-12-09 21:39:41 +0100

    Added functionality to signup and edit mail via mutation

--- app/templates/src/schema/users.js
+++ app/templates/src/schema/users.js
@@ -14,6 +14,10 @@ const userType = new GraphQLObjectType({
     username: {
       description: 'The users username.',
       type: GraphQLString
+    },
+    mail: {
+      description: 'The users E-Mail address.',
+      type: GraphQLString
     }
   })
 });
@@ -36,5 +40,41 @@ const _list = {
   }
 };
 
+const _updateMail = {
+  description: 'Update your mail address.',
+  type: userType,
+  args: {
+    mail: {
+      description: 'The Mail address to set.',
+      type: GraphQLString
+    }
+  },
+  resolve(parentValue, _, { rootValue: { session } }) {
+    if(session.passport) {
+      return users.updateMail(session.passport.user._id, _.mail);
+    }
+  }
+}
+
+const _signup = {
+  description: 'Signup for a user account.',
+  type: userType,
+  args: {
+    username: {
+      description: 'The chosen username.',
+      type: GraphQLString
+    },
+    password: {
+      description: 'The chosen password.',
+      type: GraphQLString
+    }
+  },
+  resolve(parentValue, _, { rootValue: { data } }) {
+    return users.signup(_.username, _.password);
+  }
+}
+
 export const selfField = _self;
 export const userListField = _list;
+export const userUpdateMailField = _updateMail;
+export const userSignupField = _signup;
