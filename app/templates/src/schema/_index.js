e2313ae78a5e4f3fa5b8833f82a3b88bb88b79d0
stylesuxx <stylesuxx@gmail.com>
2015-12-09 21:41:23 +0100

    Added mutations for signup and email change

--- app/templates/src/schema/_index.js
+++ app/templates/src/schema/_index.js
@@ -1,6 +1,11 @@
 import { GraphQLObjectType, GraphQLSchema } from 'graphql';
 import { getItemField } from './items';<% if (authLocal) { %>
-import { selfField, userListField } from './users';<% } %>
+import {
+  selfField,
+  userListField,
+  userSignupField,
+  userUpdateMailField
+} from './users';<% } %>
 
 const queryType = new GraphQLObjectType({
   name: 'Query',
@@ -10,7 +15,16 @@ const queryType = new GraphQLObjectType({
     users: userListField<% } %>
   })
 });
+<% if (authLocal) { %>
+  const mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    signup: userSignupField,
+    updateEmail: userUpdateMailField
+  })
+});<% } %>
 
 export const Schema = new GraphQLSchema({
-  query: queryType
+  query: queryType<% if (authLocal) { %>,
+  mutation: mutationType<% } %>
 });
