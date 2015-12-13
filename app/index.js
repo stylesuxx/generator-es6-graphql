7f20ad78431c6ec3d19300c6246c65533153898f
stylesuxx <stylesuxx@gmail.com>
2015-12-13 15:08:26 +0100

    Graphql route defaults to /

--- app/index.js
+++ app/index.js
@@ -30,7 +30,7 @@ module.exports = generator.Base.extend({
         type: 'input',
         name: 'graphqlroute',
         message: 'Route to the GraphQL endpoint',
-        default: '/graphql'
+        default: '/'
       }, function(answers) {
         this.graphqlroute = answers.graphqlroute;
         if(this.graphqlroute[0] != '/') {
