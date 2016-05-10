019a2977d40ac583f2ceef04bd693201d0f652fa
Chris Landa <stylesuxx@gmail.com>
2016-05-10 18:17:11 +0200

    Added 'createdAt' field, fixes #5

--- app/templates/src/models/User.js
+++ app/templates/src/models/User.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
+  createdAt: { type: Date },
   updatedAt: { type: Date },
   mail: { type: String }
 });
